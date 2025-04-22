b5480c22906bc4d733309243fb7324e5546b7ec4
Emilio <emiquinn23@icloud.com>
2025-04-22 00:31:50 -0500

    Created JDBC and some test queries

---
+++ src/DBTicketConnection.java
@@ -0,0 +1,24 @@
+import java.sql.*;
+
+public class DBTicketConnection {
+    public static void main(String[] args) {
+        String url = "jdbc:mysql://localhost:3306/utsa_ticket_reservation_system";
+        String username = "root";
+        String password = "Jordan is the GOAT23!";
+        try{
+            Connection conn = DriverManager.getConnection(url, username, password);
+            System.out.println("~.~.~.~ Connected to database successfully! ~.~.~.~");
+
+            Statement stmt = conn.createStatement();
+            ResultSet rs = stmt.executeQuery("SELECT * FROM Users");
+
+            while (rs.next()){ System.out.println(rs.getString("FirstName")); }
+
+            rs.close();
+            stmt.close();
+            conn.close();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+}
